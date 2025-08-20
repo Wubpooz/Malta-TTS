abb4e9f1aad197c4eca4ab8241314e96524f5672
Wubpooz <mathieu.waharte@gmail.com>
2025-08-20 13:04:41 +0200

    trainer compatibility fix

--- FineTuning/NewLanguage/compatibility.py
+++ FineTuning/NewLanguage/compatibility.py
@@ -3,6 +3,9 @@ import sys
 
 try:
     import TTS
+    import trainer
 except ImportError:
     import coqui_tts
     sys.modules["TTS"] = coqui_tts
+    import coqui_tts_trainer
+    sys.modules["trainer"] = coqui_tts_trainer
